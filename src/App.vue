70bd5b82ed8e7f025b7a79e2b881f8c3f51abac4
Saimon397 <simonecarrabba@gmail.com>
2022-11-09 16:37:12 +0100

    Finito_+_bonus

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>{{ text }}</h1>
+  <div class="my-jumbo">
+    <h1><img src="/favicon.ico" alt="logo-vue"> {{ text }}</h1>
   </div>
 </template>
 
@@ -15,5 +15,17 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.my-jumbo {
+  background-image: url(../img/1134392.png);
+  height: 100vh;
+}
 
+h1 {
+  text-align: center;
+  font-size: 45px;
+  font-style: oblique;
+  padding: 300px;
+  color: white;
+  font-weight: bold;
+}
 </style>
