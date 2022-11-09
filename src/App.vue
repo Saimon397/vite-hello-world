54d8bf79d15972dbe7ebadc11270bf41b8365060
Saimon397 <simonecarrabba@gmail.com>
2022-11-09 15:46:59 +0100

    Es.finito

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "La mia prima app con Vite!"
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
