3d5eeddf40c61ffcebb21997276e9bc9b89f987d
Saimon397 <simonecarrabba@gmail.com>
2022-11-11 20:37:37 +0100

    es_corretto

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
 <template>
-  <div class="my-jumbo">
-    <h1><img src="/favicon.ico" alt="logo-vue"> {{ text }}</h1>
+  <div>
+    <MainComponent />
   </div>
 </template>
 
 <script>
+import MainComponent from './components/MainComponent.vue';
 export default {
-  data() {
-    return {
-      text: "La mia prima app con Vite!"
-    }
-  }
+  components: { MainComponent }
 }
 </script>
 
-<style lang="css" scoped>
-.my-jumbo {
-  background-image: url(../img/1134392.png);
-  height: 100vh;
-}
+<style lang="scss" scoped>
 
-h1 {
-  text-align: center;
-  font-size: 45px;
-  font-style: oblique;
-  padding: 300px;
-  color: white;
-  font-weight: bold;
-}
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="my-jumbo">
+        <h1><img src="/favicon.ico" alt="logo-vue"> {{ text }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainComponent',
+    data() {
+        return {
+            text: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.my-jumbo {
+    background-image: url(../img/1134392.png);
+    height: 100vh;
+}
+
+h1 {
+    text-align: center;
+    font-size: 45px;
+    font-style: oblique;
+    padding: 300px;
+    color: white;
+    font-weight: bold;
+}
+</style>
